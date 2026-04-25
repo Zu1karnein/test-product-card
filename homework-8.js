09cbc17ff78cdce75efe9461b6f74944647cf1af
Zu1karnein <jak.edmon@gmail.com>
2026-04-26 01:47:31 +0300

    added template of cards

--- homework-8.js
+++ homework-8.js
@@ -1,9 +1,26 @@
-students[
-  {
-    name: "Ибрагим",
-    surname: "Абдурагимов",
-    age: 15,
-    country: "Дагестан",
-    height: 179,
-  }
-];
+import { productCards } from "./product-cards.js";
+
+console.log("JS WORKS");
+
+const cardTemplate = document.getElementById("card-template");
+const cardsSection = document.getElementById("cards-section");
+
+productCards.forEach((card) => {
+  const cardClone = cardTemplate.content.cloneNode(true);
+
+  cardClone.querySelector(".title").textContent = card.title;
+  cardClone.querySelector(".description").textContent = card.description;
+
+  cardClone.querySelector(".firstIngredient").textContent =
+    card.firstIngredient;
+  cardClone.querySelector(".secondIngredient").textContent =
+    card.secondIngredient;
+  cardClone.querySelector(".thirdIngredient").textContent =
+    card.thirdIngredient;
+
+  cardClone.querySelector(".priceP").innerHTML = card.price || card.priceP;
+
+  cardClone.querySelector(".img-card").src = card.image;
+
+  cardsSection.appendChild(cardClone);
+});
