5460f139c1a6aa246a87c69850e95e14f6269575
Zu1karnein <jak.edmon@gmail.com>
2026-04-28 12:31:59 +0300

    added buttons from script.js, script.js was deleted

--- homework-8.js
+++ homework-8.js
@@ -2,11 +2,15 @@ import { productCards } from "./product-cards.js";
 
 console.log("JS WORKS");
 
+// template
 const cardTemplate = document.getElementById("card-template");
 const cardsSection = document.getElementById("cards-section");
 
 productCards.forEach((card) => {
   const cardClone = cardTemplate.content.cloneNode(true);
+
+  const article = cardClone.querySelector("article");
+  article.className = card.name;
 
   cardClone.querySelector(".title").textContent = card.title;
   cardClone.querySelector(".description").textContent = card.description;
@@ -23,6 +27,57 @@ productCards.forEach((card) => {
   cardClone.querySelector(".img-card").src = card.image;
 
   cardsSection.appendChild(cardClone);
+});
 
-  console.log(card);
+// BUTTONS
+// recolor first card
+const firstCardRecolorBtn = document.getElementById("recolor-first-card-btn");
+const firstCard = document.querySelector(".card__mousse");
+const blueHashColor = "#96ffef";
+
+firstCardRecolorBtn.addEventListener("click", () => {
+  if (firstCard) {
+    firstCard.style.backgroundColor = blueHashColor;
+  } else {
+    console.log("first card not found");
+  }
+});
+
+// recolor all cards
+const catalogCards = document.querySelectorAll(".card");
+const allCardsRecolorBtn = document.getElementById("recolor-all-cards-btn");
+const greenHashColor = "#abff96";
+
+allCardsRecolorBtn.addEventListener("click", () => {
+  catalogCards.forEach((card) => (card.style.backgroundColor = greenHashColor));
+});
+
+// open google
+const googleOpenBtn = document.getElementById("open-google-btn");
+googleOpenBtn.addEventListener("click", openGoogle);
+
+function openGoogle() {
+  const answer = confirm(
+    "Вы действительно хотите открыть перейти на сайт google.com?",
+  );
+
+  if (answer) {
+    window.open("http://google.com");
+  } else {
+    console.log("Пользователь отменил переход на сайт google.com");
+  }
+}
+
+// main title to console
+const mainTitle = document.querySelector(".main_title");
+
+mainTitle.addEventListener("mouseenter", () => {
+  console.log(mainTitle.textContent);
+});
+
+// change color of button
+const himselfRecolorBtn = document.getElementById("recolor-himself-btn");
+
+himselfRecolorBtn.addEventListener("click", () => {
+  himselfRecolorBtn.classList.toggle("blue-bg");
 });
